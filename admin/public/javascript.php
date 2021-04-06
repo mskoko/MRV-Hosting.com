d8332ec73d72d58d0e3b3c129a5cac572372aea8
Muhamed Skoko <52577153+mskoko@users.noreply.github.com>
2021-04-06 23:24:43 +0200

    Delete admin directory

--- admin/public/javascript.php
+++
@@ -1,8 +0,0 @@
-<!-- JavaScript files-->
-	<script src="/<?php echo $admDir; ?>/vendor/jquery/jquery.min.js"></script>
-	<script src="/<?php echo $admDir; ?>/vendor/popper.js/umd/popper.min.js"> </script>
-	<script src="/<?php echo $admDir; ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="/<?php echo $admDir; ?>/vendor/jquery.cookie/jquery.cookie.js"> </script>
-	<script src="/<?php echo $admDir; ?>/vendor/chart.js/Chart.min.js"></script>
-	<script src="/<?php echo $admDir; ?>/vendor/jquery-validation/jquery.validate.min.js"></script>
-	<script src="/<?php echo $admDir; ?>/js/front.js"></script>
